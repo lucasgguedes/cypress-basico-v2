38231077009370b0d47e7e4a88ac6089f831a071
Lucas Guedes <lucas.g.gueedes@hotmail.com>
2023-10-03 20:54:26 -0300

    Teste de validacao

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,14 +10,45 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário',function(){
+        
+        const longText = 'Teste, teste , teste Teste, teste , teste Teste, teste , teste Teste, teste , teste Teste, teste , teste Teste, teste , teste Teste, teste , teste Teste, teste , teste Teste, teste , teste '
         cy.get('#firstName').type('Lucas')
         cy.get('#lastName').type('Guedes')
         cy.get('#email').type('lucas@teste.com.br')
-        cy.get('#open-text-area').type('teste 1234')
+        cy.get('#open-text-area').type(longText, {delay: 0})
+        cy.get('.button[type="submit"]').click()
+        
+        cy.get('.success').should('be.visible')
+    })
+
+    //inserindo e-mail com formatacao invalida
+    it('Mensagem de erro, e-mail invalido', function(){
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Guedes')
+        cy.get('#email').type('lucas@teste,com.br')
+        cy.get('#open-text-area').type('teste')
         cy.get('.button[type="submit"]').click()
 
-        cy.get('.success').should('be.visible')
+        cy.get('.error').should('be.visible')
+    })
+//inserindo telefone com erro
+    it('Erro telefone', function(){
+        
+        cy.get('#phone').type('lucasteste')
+            .should('have.value', '')
+    })
+//marcando informação obrigatória e não preenchendo
+    it.only('Mensagem de erro q uando o telefone é obrigatório, mas não é preenchido', function(){
+        
+        cy.get('#phone-checkbox').click()
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Guedes')
+        cy.get('#email').type('lucas@teste.com.br')
+        cy.get('#open-text-area').type('teste')
+        cy.get('.button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
     })
 })
 
