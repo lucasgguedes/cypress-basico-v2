99ac772dfa874c4a3204d8d980e5dd75861bd3f2
Lucas Guedes <lucas.g.gueedes@hotmail.com>
2023-10-20 12:12:09 -0300

    Atualizacao

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -39,13 +39,27 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('have.value', '')
     })
 //marcando informação obrigatória e não preenchendo
-    it.only('Mensagem de erro q uando o telefone é obrigatório, mas não é preenchido', function(){
+    it('Mensagem de erro q uando o telefone é obrigatório, mas não é preenchido', function(){
         
         cy.get('#phone-checkbox').click()
         cy.get('#firstName').type('Lucas')
         cy.get('#lastName').type('Guedes')
         cy.get('#email').type('lucas@teste.com.br')
         cy.get('#open-text-area').type('teste')
+        cy.get('.button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+    })
+//preenche informações, valida o campo e depois apaga
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        cy.get('#firstName').type('Lucas').should('have.value', 'Lucas').clear().should('have.value', '')
+        cy.get('#lastName').type('Guedes').should('have.value', 'Guedes').clear().should('have.value', '')
+        cy.get('#email').type('lucas@teste.com.br').should('have.value', 'lucas@teste.com.br').clear().should('have.value', '')
+        cy.get('#phone').type('12345678').should('have.value', '12345678').clear().should('have.value', '')
+    })
+
+// mensagem de erro ao tentar salvar sem enviar dados
+    it('Mensagem de erro ao submeter form sem preencher campos obrigatórios', function(){
         cy.get('.button[type="submit"]').click()
 
         cy.get('.error').should('be.visible')
